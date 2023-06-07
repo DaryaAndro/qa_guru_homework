28aebbebcf9e04a6dbd2434e0df102a58c19df2f
Dasha <dbukhareva@yandex.ru>
2023-06-07 11:32:42 +0300

    Правки по ДЗ после лекции №2

--- qa_guru_python_6_2/test_google_search.py
+++ qa_guru_python_6_2/test_google_search.py
@@ -2,15 +2,20 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+
 @pytest.fixture
 def size_window():
     browser.config.window_height = 1080
     browser.config.window_width = 1920
 
-@pytest.fixture
-def chrome(size_window):
-    browser.open('https://google.com')
 
-def test_google_search_not_found(chrome):
+def test_google_search(size_window):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_google_search_not_found(size_window):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('fdfdssddfdsfdfsfd').press_enter()
     browser.element('.card-section').should(have.text('По запросу fdfdssddfdsfdfsfd ничего не найдено.'))
